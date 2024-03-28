b068de8f6286219e654649729f9f9b24bf4a171a
huangyan321 <1604549268@qq.com>
2024-03-28 22:53:35 +0800

    deps: remove unused deps

--- rollup.config.js
+++ rollup.config.js
@@ -4,15 +4,11 @@ import { defineConfig } from 'rollup';
 import ts from '@rollup/plugin-typescript';
 import json from '@rollup/plugin-json';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import polyfillNode from 'rollup-plugin-polyfill-node';
-import replace from '@rollup/plugin-replace';
 import commonjs from '@rollup/plugin-commonjs';
 export default defineConfig({
   input: ['src/cli.ts', 'src/index.ts'],
   output: {
-    /** 输出目录 */
     dir: './dist',
-    /** 输出文件为 CommonJS格式 */
     format: 'es',
   },
   watch: {
